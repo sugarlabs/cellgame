23720793097a6c791e476ddb692cbb9f3f17601b
Yash Agrawal <yagrawal900@gmail.com>
2018-02-23 02:06:17 +0530

    Ported to Gtk3 and updated sugargame to v1.2

--- activity.py
+++ activity.py
@@ -16,13 +16,18 @@
 # You should have received a copy of the GNU General Public License
 # along with Cell Management.  If not, see <http://www.gnu.org/licenses/>.
 
-import gtk
-from sugar.activity import activity
-from sugar.graphics.toolbarbox import ToolbarBox
-from sugar.activity.widgets import ActivityToolbarButton
-from sugar.activity.widgets import StopButton
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import pygame
+import sugargame
 import sugargame.canvas
-
+from sugar3.activity import activity
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.graphics.toolbutton import ToolButton
+from sugar3.activity.widgets import StopButton
+from gettext import gettext as _
 import cells
 
 class Activity(activity.Activity):
@@ -31,33 +36,37 @@ class Activity(activity.Activity):
         activity.Activity.__init__(self, handle)
 
         self.max_participants = 1
-        self.actividad = cells.Game()
+        self.game = cells.Game()
+        self.game.canvas = sugargame.canvas.PygameCanvas(
+            self,
+            main=self.game.mainloop,
+            modules=[pygame.display, pygame.font])
+        self.set_canvas(self.game.canvas)
+        self.game.canvas.grab_focus()
         self.build_toolbar()
-        self._pygamecanvas = sugargame.canvas.PygameCanvas(self)
-        self.set_canvas(self._pygamecanvas)
-        self._pygamecanvas.grab_focus()
-        self._pygamecanvas.run_pygame(self.actividad.mainloop)
 
     def build_toolbar(self):
+        
+        toolbar_box = ToolbarBox()
+        self.set_toolbar_box(toolbar_box)
+        toolbar_box.show()
 
-        toolbox = ToolbarBox()
         activity_button = ActivityToolbarButton(self)
-        toolbox.toolbar.insert(activity_button, -1)
+        toolbar_box.toolbar.insert(activity_button, -1)
         activity_button.show()
 
-        separator = gtk.SeparatorToolItem()
+        separator = Gtk.SeparatorToolItem()
         separator.props.draw = False
         separator.set_expand(True)
-        toolbox.toolbar.insert(separator, -1)
+        toolbar_box.toolbar.insert(separator, -1)
+        separator.show()
 
         stop_button = StopButton(self)
         stop_button.props.accelerator = '<Ctrl>q'
-        toolbox.toolbar.insert(stop_button, -1)
+        toolbar_box.toolbar.insert(stop_button, -1)
         stop_button.show()
 
-        self.set_toolbar_box(toolbox)
-
-        toolbox.show_all()
+        self.show_all()
 
     def read_file(self, file_path):
         pass
